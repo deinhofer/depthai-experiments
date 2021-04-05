11c990adf5a75d9c39e12412840b9950de6eb907
Martin Deinhofer <deinhofer.m@gmail.com>
2021-04-05 10:01:23 +0200

    immediately move mouse, center mouse at the beginning and added sanity check

--- gen2-gaze-estimation/mouse_controller.py
+++ gen2-gaze-estimation/mouse_controller.py
@@ -11,11 +11,22 @@ import pyautogui
 
 class MouseController:
     def __init__(self, precision, speed):
-        precision_dict={'high':100, 'low':1000, 'medium':500}
-        speed_dict={'fast':0.01, 'slow':10, 'medium':5}
+        precision_dict={'high':10, 'low':1000, 'medium':500}
+        speed_dict={'fast':0.0, 'slow':10, 'medium':5}
 
         self.precision=precision_dict[precision]
         self.speed=speed_dict[speed]
+        screen_size=pyautogui.size()
+        pyautogui.moveTo(screen_size[0]/2,screen_size[1]/2)
 
     def move(self, x, y):
-        pyautogui.moveRel(x*self.precision, -1*y*self.precision, duration=self.speed)
+        screen_pos=pyautogui.position()
+        self.print_position(screen_pos[0]+x*self.precision, screen_pos[1]+-1*y*self.precision)
+        if pyautogui.onScreen(screen_pos[0]+x*self.precision, screen_pos[1]+-1*y*self.precision):
+            pyautogui.moveRel(x*self.precision, -1*y*self.precision, duration=self.speed)
+
+    def print_position(self, x,y):
+        #x, y = pyautogui.position()
+        positionStr = 'X: {0} Y: {1}'.format(str(x).rjust(4), str(y).rjust(4))
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
